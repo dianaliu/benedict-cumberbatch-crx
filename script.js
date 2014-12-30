d5f9b9c6e5463ba88c5b06eccff00c49bb52da6a
Diana Liu <diana.g.liu@gmail.com>
2014-12-30 16:33:23 -0500

    First commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 var name = getName(),
-    lastName = name.split(' ')[1];      
+    lastName = name.split(' ')[1];
 
+// Stolen from www.beesgo.biz/chrome.html
 walk(document.body);
 
 function walk(node) {
   // I stole this function from here:
   // http://is.gd/mwZp7E
-  
+
   var child, next;
 
-  switch ( node.nodeType )  
+  switch ( node.nodeType )
   {
     case 1:  // Element
     case 9:  // Document
@@ -33,25 +34,25 @@ function handleText(textNode) {
 
   v = v.replace(/\bBenedict Cumberbatch\b/gi, name);
   v = v.replace(/\bCumberbatch\b/gi, lastName);
-  
+
   textNode.nodeValue = v;
 }
 
 // Stolen from http://benedictcumberbatchgenerator.tumblr.com/
-// I was going to make a request for each call, but tumblr doesn't support https
+// I was going to make a request for this, but tumblr doesn't support https
 // Sorry!
 function getName(){
   var firstnamelist = ["Bumblebee", "Bandersnatch", "Broccoli", "Rinkydink", "Bombadil", "Boilerdang", "Bandicoot", "Fragglerock", "Muffintop", "Crumplesack", "Congleton", "Blubberdick", "Buffalo", "Benadryl", "Butterfree", "Burberry", "Whippersnatch", "Buttermilk", "Beezlebub", "Budapest", "Boilerdang", "Blubberwhale", "Bumberstump", "Bulbasaur", "Cogglesnatch", "Liverswort", "Bodybuild", "Johnnycash", "Bendydick", "Burgerking", "Bonaparte", "Bunsenburner", "Billiardball", "Bukkake", "Baseballmitt", "Blubberbutt", "Baseballbat", "Rumblesack", "Barister", "Danglerack", "Rinkydink", "Bombadil", "Honkytonk", "Billyray", "Bumbleshack", "Snorkeldink", "Anglerfish", "Beetlejuice", "Bedlington", "Bandicoot", "Boobytrap", "Blenderdick", "Bentobox", "Anallube", "Pallettown", "Wimbledon", "Buttercup", "Blasphemy", "Syphilis", "Snorkeldink", "Brandenburg", "Barbituate", "Snozzlebert", "Tiddleywomp", "Bouillabaisse", "Wellington", "Benetton", "Bendandsnap", "Timothy", "Brewery", "Bentobox", "Brandybuck", "Benjamin", "Buckminster"];
   var lastnamelist = ["Coddleswort", "Curdlesnoot", "Calldispatch", "Humperdinck", "Rivendell", "Cuttlefish", "Lingerie", "Vegemite", "Ampersand", "Cumberbund", "Candycrush", "Clombyclomp", "Cragglethatch", "Nottinghill", "Cabbagepatch", "Camouflage","Creamsicle", "Curdlemilk", "Upperclass", "Frumblesnatch", "Crumplehorn", "Talisman", "Candlestick", "Chesterfield", "Bumbersplat", "Scratchnsniff", "Snugglesnatch", "Charizard", "Carrotstick", "Cumbercooch", "Crackerjack", "Crucifix", "Cuckatoo", "Cockletit", "Collywog", "Capncrunch", "Covergirl", "Cumbersnatch", "Countryside","Coggleswort", "Splishnsplash", "Copperwire", "Animorph", "Curdledmilk", "Cheddarcheese", "Cottagecheese", "Crumplehorn", "Snickersbar", "Banglesnatch", "Stinkyrash", "Cameltoe", "Chickenbroth", "Concubine", "Candygram", "Moldyspore", "Chuckecheese", "Cankersore", "Crimpysnitch", "Wafflesmack", "Chowderpants", "Toodlesnoot", "Clavichord", "Cuckooclock", "Oxfordshire", "Cumbersome", "Chickenstrips", "Battleship", "Commonwealth", "Cunningsnatch", "Custardbath", "Kryptonite", "Curdlesnoot", "Cummerbund", "Coochyrash"];
-  
+
   //the specials ;) ;)
   var fullnamelist = ["Wimbledon Tennismatch", "Rinkydink Curdlesnoot", "Butawhiteboy Cantbekhan", "Benadryl Claritin", "Bombadil Rivendell", "Wanda's Crotchfruit", "Wanda's Crotchfruit", "Biblical Concubine", "Butawhiteboy Cantbekhan", "Syphilis Cankersore", "Butawhiteboy Cantbekhan", "Benedict Timothy Carlton Cumberbatch", "Candycrush Templerun", "Wanda's Son", "Buckminster Fullerene", "Canon Korrasami"];    var random1 = parseInt(Math.random() * firstnamelist.length);
   var random2 = parseInt(Math.random() * lastnamelist.length);
-  var randomfull = parseInt(Math.random() * fullnamelist.length);   
+  var randomfull = parseInt(Math.random() * fullnamelist.length);
 
   var name = firstnamelist[random1] + " " + lastnamelist[random2];
   var fullname = fullnamelist[randomfull];
-  
+
   var numberroll = Math.floor(Math.random() * 10) + 1;
   var finalname = "";
 
@@ -62,4 +63,4 @@ function getName(){
   }
 
   return finalname;
-} 
+}
